cd7c4b11edddea6fe27eab8ec063a1784dc156d8
Devikesh Shende <61633788+Devikesh01@users.noreply.github.com>
2020-08-19 19:58:11 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,38 +1,20 @@
-def addition(no1,no2):
-    return  int(no1)+int(no2)
+from flask import Flask
 
-def subtraction(no1,no2):
-    return int(no1)-int(no2)
+app = Flask(__name__)
 
-def multiplication(no1,no2):
-    return int(no1)*int(no2)
-
-def division(no1,no2):
-    if(int(no2) == 0):
-        print("Cant divide by 0")
+@app.route('/')
+def cal_fun(var_1=6, var_2=3, operation = "Addition" ):
+    if(operation == 'Addition'):
+        result = var_1 + var_2
+    elif(operation == 'Subtraction'):
+        result = var_1 - var_2
+    elif(operation == 'Multiplication'):
+        result = var_1 * var_2
+    elif(operation == 'Division'):
+        result = var_1 / var_2
     else:
-         return int(no1)/int(no2)
+        result = 'INVALID CHOICE'
+    return result
 
-no1 = input("Please enter the first number: ")
-no2 = input("Please enter the second number: ")
-
-operation = input("Please enter the operation to be performed: ")
-
-if(operation == "A"):
-    iret = addition(no1,no2)
-    print("The addition is "+ str(iret))
-
-elif(operation == "S"):
-    iret = subtraction(no1,no2)
-    print("The subtraction is "+ str(iret))
-
-elif(operation == "M"):
-    iret = multiplication(no1,no2)
-    print("The multiplication is "+str(iret))
-
-elif(operation == "D"):
-    iret = division(no1,no2)
-    print("The Division is "+str(iret))
-
-else:
-    print("Please enter a valid operation")
+if __name__ == '__main__':
+    app.run()
